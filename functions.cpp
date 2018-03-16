bc8685453b1afdd205957c604a3ad37e70ba0474
Grant Ferrell <fgrant@bgsu.edu>
2018-03-16 12:00:12 -0400

    File Complete & Compiles

--- functions.cpp
+++ functions.cpp
@@ -10,11 +10,20 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <assert.h>
 
-// Generate Datasets Function (Phase-1)
-void generateSets(string fileName, int size) {
+// Prototypes
+void generateSets(std::string, int);
+void insertionSort(int [],int);
+void quickSort(int [], int, int);
+
+// Globals for the Number of Comparisons
+int numComparInsert = 0, numComparQuick = 0;
+
+// Generate Datasets Function (Complete)
+void generateSets(std::string fileName, int size) {
   // Inital Varibles
-  ofstream inFile;
+  std::ofstream inFile;
   inFile.open(fileName);
   // Seed to current Time
   srand(time(NULL));
@@ -25,13 +34,14 @@ void generateSets(string fileName, int size) {
     inFile << rand() % size;
     inFile << ","; // Splits Data
     }
+    inFile << "."; // States end of Set
     inFile << "\n"; // Seperates sets by line
   }
   // Close File
   inFile.close();
 }
 
-// Insertion Sort Function (Phase-1)
+// Insertion Sort Function (Complete)
 void insertionSort(int array[], int size) {
   // Sorting
   for (int i = 1; i < size; ++i) {
@@ -39,8 +49,10 @@ void insertionSort(int array[], int size) {
     int j = i - 1;
     while (j >= 0 && array[j] > temp) {
       array[j + 1] = array[j];
-      j = j - 1;
+      --j;
+      ++numComparInsert; // Increment Comparisons
     }
+    ++numComparInsert; // Increment Comparisons
     array[j + 1] = temp;
   }
 }
@@ -59,6 +71,7 @@ int partition (int array[], int lower, int upper) {
       array[i] = array[j];
       array[j] = temp;
     }
+    ++numComparQuick; // Increment Comparisons
   }
   temp = array[i + 1];
   array[i + 1] = array[upper];
@@ -66,7 +79,7 @@ int partition (int array[], int lower, int upper) {
   return (i + 1);
 }
 
-// Quick Sort Function (Phase-1) [Inital call with - array, 0, size]
+// Quick Sort Function (Complete) [Inital call with - array, 0, size - 1]
 void quickSort(int array[], int lower, int upper) {
   if (lower < upper) {
     // Partition index
@@ -78,32 +91,49 @@ void quickSort(int array[], int lower, int upper) {
 }
 
 // Main (Command Line Interpretation) [D - Generation of Data sets, S - Sorting the Data]
-int main(int argc, char **argv[]) {
+int main(int argc, char* argv[]) {
   // Read in Arguments
   if (argv[1] == "D") {
     // Call Generate Values function
-    generateSets("insertionSets.txt", int 1000);
-    generateSets("quickSets.txt", int 50000);
+    generateSets("insertionSets.txt", 1000);
+    generateSets("quickSets.txt", 50000);
   }
   else if (argv[1] == "S") {
     // Sorts Data using functions
     // Inital Varibles
-    ofstream inFile;
-    ifstream outFile;
-    string line, format;
-    string insertionOutput[10], quickOutput[10];
-    int dataSetInsertion[1000];
+    std::ofstream inFile;
+    std::ifstream outFile;
+    std::string line, format, output;
+    std::string insertionOutput[10], quickOutput[10];
+    int dataSetInsertion[1000], dataSetQuick[50000];
+    int start = 0, temp = 0;
+    double cpuTime;
+    std::clock_t clockStart;
 
     // I for 10 sets of 1k
     outFile.open("insertionSets.txt"); // Open File
     if (outFile.is_open()) {
       for (int i = 1; i <= 10; ++i) {
         getline(outFile, line);
-        // Create an array
-        // Sort the array
-        // Get Number of Comparisons
-        // Compute Average CPU time
-        // Write info to a String! use (+) for concate'
+        // Seperates Data to be Inserted into arrays
+        for (int j = 0, z = 0; line.substr(start, j) == "." && z == 1000; ++j) {
+          if (line.substr(j, j) == ",") {
+            // Insert steps
+            temp = stoi(line.substr(start, j - 1));
+            dataSetInsertion[j] = temp;
+            ++z;
+            start = j + 1;
+          }
+        }
+        clockStart = std::clock();
+        insertionSort(dataSetInsertion, 1000); // Sort Data
+        // Calculate CPU time
+        cpuTime = (std::clock() - clockStart) / (double)CLOCKS_PER_SEC;
+        // Write info to a std::string! use (+) for concate'
+        output = "I," + std::to_string(i) + ",1000," + std::to_string(numComparInsert) 
+          + "," + std::to_string(cpuTime) + ",1000000"; // Worst Case is big O n^2
+        // Add std::string to Output Array
+        insertionOutput[i] = output;
       }
     }
     else {
@@ -115,11 +145,25 @@ int main(int argc, char **argv[]) {
     if (outFile.is_open()) {
       for (int i = 1; i <= 10; ++i) {
         getline(outFile, line);
-        // Create an array
-        // Sort the array
-        // Get Number of Comparisons
-        // Compute Average CPU time
-        // Write info to a String! use (+) for concate'
+     // Seperates Data to be Inserted into arrays
+        for (int j = 0, z = 0; line.substr(start, j) == "." && z == 50000; ++j) {
+          if (line.substr(j, j) == ",") {
+            // Insert steps
+            temp = stoi(line.substr(start, j - 1));
+            dataSetQuick[j] = temp;
+            ++z;
+            start = j + 1;
+          }
+        }
+        clockStart = std::clock();
+        quickSort(dataSetQuick, 0, 49999); // Sort Data
+        // Calculate CPU time
+        cpuTime = (std::clock() - clockStart) / (double)CLOCKS_PER_SEC; 
+        // Write info to a std::string! use (+) for concate'
+        output = "Q," + std::to_string(i) + ",50000," + std::to_string(numComparQuick) 
+          + "," + std::to_string(cpuTime) + ",2500000000"; //Worst Case is Theta n^2
+        // Add std::string to Output Array
+        quickOutput[i] = output;
       }
     }
     else {
@@ -128,9 +172,17 @@ int main(int argc, char **argv[]) {
     outFile.close();
     // Write Output arrays to File [seperated with commas and lines]
     inFile.open("Excel_Data.txt"); // Open File
-    for (int i = 1; i <= 10; ++i){
-      
+    // Write Insertion
+    for (int i = 1; i <= 10; ++i) {
+      inFile << insertionOutput[i];
+      inFile << "\n";
     }
+    // Write Quick
+    for (int i = 1; i <= 10; ++i) {
+      inFile << quickOutput[i];
+      inFile << "\n";
+    }
+    inFile.close();
   }
   else {
     // Do nothing
